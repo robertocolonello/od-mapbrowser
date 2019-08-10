cee2badef1ee717d30cf970ac59153352712f5a1
Roberto Colonello <roberto@parsec.it>
2019-08-10 10:42:31 +0200

    init

---
+++ datasets.js
@@ -0,0 +1,15 @@
+loadDatasets({
+    "common": {
+	"url": "http://tourism.opendatahub.bz.it/api/"
+    },
+    "datasets": {
+        "Accommodation": {"id":"Id", "name":"Shortname", "color":"31,119,180"},
+        "MetaRegion":  {"id":"Id", "name":"Detail.it.Title", "color":"255,127,14"},
+        "ExperienceArea":  {"id":"Id", "name":"Detail.it.Title", "color":"44,160,44"},
+        "TourismAssociation":  {"id":"Id", "name":"Detail.it.Title", "color":"214,39,40"},
+        "Municipality":  {"id":"Id", "name":"Detail.it.Title", "color":"148,103,189"},
+        "District":  {"id":"Id", "name":"Detail.it.Title", "color":"140,86,75"},
+        "SkiRegion":  {"id":"Id", "name":"Detail.it.Title", "color":"188,189,34"},
+        "SkiArea":  {"id":"Id", "name":"Detail.it.Title", "polygon": "GpsPolygon", "color":"23,190,207"}
+    }
+});
